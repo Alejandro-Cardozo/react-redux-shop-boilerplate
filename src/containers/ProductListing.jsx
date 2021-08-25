792b1db352b058a3d79e32f682f463cfd882d632
Alejandro Cardozo <uri.cardozo@gmail.com>
2021-08-25 11:26:31 -0300

    Accessing state through useSelector and fetching data with axios

--- src/containers/ProductListing.jsx
+++ src/containers/ProductListing.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import axios from 'axios';
+
+import ProductComponent from './ProductComponent';
 
 const ProductListing = () => {
+  const products = useSelector((state) => state);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const response = await axios.get('https://fakestoreapi.com/products');
+        console.log(response.data);
+      } catch (error) {
+        console.log("Err", error);
+      }
+    };
+
+    getProducts();
+  }, []);
+
   return (
-    <div>
-      <h1>ProductListing</h1>
+    <div className='ui grid container'>
+      <ProductComponent />
     </div>
   );
 };
