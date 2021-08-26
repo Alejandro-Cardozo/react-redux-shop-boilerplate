e44ba36dd33e19347b2bb58484455395eeaa8de5
Alejandro Cardozo <uri.cardozo@gmail.com>
2021-08-26 16:31:45 -0300

    loader added

--- src/containers/ProductDetail.jsx
+++ src/containers/ProductDetail.jsx
@@ -1,49 +1,50 @@
-
-import React, { useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import {
   fetchProducts,
   removeSelectedProduct,
-} from "../redux/actions/productActions";
+} from '../redux/actions/productActions';
 const ProductDetails = () => {
   const { productId } = useParams();
 
   let product = useSelector((state) => state.product);
   const { id, image, title, price, category, description } = product;
-  
+
   const dispatch = useDispatch();
 
   useEffect(() => {
-    if (productId && productId !== "") dispatch(fetchProducts(productId));
+    if (productId && productId !== '') dispatch(fetchProducts(productId));
     return () => {
       dispatch(removeSelectedProduct());
     };
   }, [productId, dispatch]);
   return (
-    <div className="ui grid container">
+    <div className='ui grid container'>
       {Object.keys(product).length === 0 ? (
-        <div>...Loading</div>
+          <div class="ui active centered inline loader"></div>
       ) : (
-        <div className="ui placeholder segment">
-          <div className="ui two column stackable center aligned grid">
-            <div className="ui vertical divider">AND</div>
-            <div className="middle aligned row">
-              <div className="column lp">
-                <img className="ui fluid image" src={image} alt={title} />
+        <div className='ui placeholder segment'>
+          <div className='ui two column stackable center aligned grid'>
+            <div className='ui vertical divider'>AND</div>
+            <div className='middle aligned row'>
+              <div className='column lp'>
+                <img className='ui fluid image' src={image} alt={title} />
               </div>
-              <div className="column rp">
+              <div className='column rp'>
                 <h1>{title}</h1>
                 <h2>
-                  <a className="ui teal tag label" href={`/products/${id}`}>${price}</a>
+                  <a className='ui teal tag label' href={`/products/${id}`}>
+                    ${price}
+                  </a>
                 </h2>
-                <h3 className="ui brown block header">{category}</h3>
+                <h3 className='ui brown block header'>{category}</h3>
                 <p>{description}</p>
-                <div className="ui vertical animated button" tabIndex="0">
-                  <div className="hidden content">
-                    <i className="shop icon"></i>
+                <div className='ui vertical animated button' tabIndex='0'>
+                  <div className='hidden content'>
+                    <i className='shop icon'></i>
                   </div>
-                  <div className="visible content">Add to Cart</div>
+                  <div className='visible content'>Add to Cart</div>
                 </div>
               </div>
             </div>
